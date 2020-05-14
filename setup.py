9f779f3405d82f91dcb0d717011e5de37c1c65bb
Velythyl <charlie.gauthier@umontreal.ca>
2020-05-14 15:36:08 -0400

    fixed pyglet

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='duckietown_rl',
       version='1.0',
-      install_requires=['gym>=0.5',
+      install_requires=['gym==0.15.4',
                         'gym_duckietown_agent>=2018.08',
                         'hyperdash', # for logging
                         'sklearn',
