a72bd7c2841d008caca94482c4660f84795392f1
Andrea Censi <acensi@ethz.ch>
2021-06-16 12:02:06 +0200

    bumpversion

--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
 from setuptools import setup
 
+
+
+def get_version(filename: str):
+    import ast
+
+    version = None
+    with open(filename) as f:
+        for line in f:
+            if line.startswith("__version__"):
+                version = ast.parse(line).body[0].value.s
+                break
+        else:
+            raise ValueError("No version found in %r." % filename)
+    if version is None:
+        raise ValueError(filename)
+    return version
+
+
+version = get_version(filename="duckietown_rl/__init__.py")
+
+
 setup(name='duckietown_rl',
       version='1.0',
+      packages=['duckietown_rl'],
       install_requires=['gym==0.15.4',
                         'gym_duckietown_agent>=2018.08',
                         'hyperdash', # for logging
